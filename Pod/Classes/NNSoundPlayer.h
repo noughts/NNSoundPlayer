33f29324a5321712942b4460057cdf0190d072be
Koichi Yamamoto <noughts@gmail.com>
2016-03-16 17:28:56 +0900

    a

--- Pod/Classes/NNSoundPlayer.h
+++ Pod/Classes/NNSoundPlayer.h
@@ -1,3 +1,13 @@
+/*
+   音声が再生されない時には、以下の点をチェックしてみましょう。
+ 
+   - NNSoundPlayer がインスタンスのメンバとして定義されているか？
+     関数内で初期化してもすぐにdeallocされてしまいます。
+ 
+ //音声ファイルがロードできません。Project Navigator でファイルを選択し、File Inspector の Target Membership で利用したいTargetにチェックが入っているか確認しましょう。
+*/
+
+
 @import Foundation;
 @import AudioToolbox;
 
