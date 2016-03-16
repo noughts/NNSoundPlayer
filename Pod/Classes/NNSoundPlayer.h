594642c6641d981a71e3bf75fa0a7ed31316ff6d
Koichi Yamamoto <noughts@gmail.com>
2016-03-16 16:37:24 +0900

    a

---
+++ Pod/Classes/NNSoundPlayer.h
@@ -0,0 +1,13 @@
+//
+//  NNSoundPlayer.h
+//  Pods
+//
+//  Created by noughts on 2016/03/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NNSoundPlayer : NSObject
+
+@end
---
+++ Pod/Classes/NNSoundPlayer.m
@@ -0,0 +1,13 @@
+//
+//  NNSoundPlayer.m
+//  Pods
+//
+//  Created by noughts on 2016/03/16.
+//
+//
+
+#import "NNSoundPlayer.h"
+
+@implementation NNSoundPlayer
+
+@end
